a34640a18bfb9c16815323ca25e5aea260c24169
fajrinnr <fajrin.noorrachman11@gmail.com>
2021-04-05 09:17:57 +0700

    Create Page for List Pokemons - Create Page - Updated Style Global

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,40 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import { getPokemonsFetchQuery } from "../src/graphql/getPokemons";
+import { useEffect, useState } from "react";
+/** @jsxImportSource @emotion/react */
+import { css, jsx } from "@emotion/react";
+import MainLayout from "../src/layouts/mainLayout";
+import CardPokemon from "../src/components/cardPokemon";
+import Link from "next/link";
+import { route } from "../src/helpers/route";
 
-export default function Home() {
+export default function Home({ pokemons }) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
-  )
+    <MainLayout>
+      <div
+        css={css`
+          display: flex;
+          flex-direction: row;
+          justify-content: center;
+          align-items: center;
+          flex-wrap: wrap;
+        `}
+      >
+        {pokemons.results.map((pokemon, i) => {
+          return (
+            <CardPokemon key={i} name={pokemon.name} image={pokemon.image} />
+          );
+        })}
+      </div>
+    </MainLayout>
+  );
 }
+
+export const getServerSideProps = async (ctx) => {
+  // Fetch data from external API
+  const pokemonListResponse = await getPokemonsFetchQuery({
+    limit: 10,
+  });
+  console.log(pokemonListResponse);
+  // Pass data to the page via props
+  return { props: { pokemons: pokemonListResponse.pokemons } };
+};
